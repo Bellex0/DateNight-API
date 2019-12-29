0990b5d27c349e90658952d823ae50700e705eb2
bellex0 <bellepx3@gmail.com>
2019-12-29 05:12:28 -0500

    add more validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,6 @@ class User < ApplicationRecord
     
     has_secure_password
     validates :username, uniqueness: { case_sensitive: false }
+    validates :username, :name, :location, presence: true
     # mount_uploader :image, ImageUploader
 end
